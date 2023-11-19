03e65fcc94211e23a7a69a154a52f08d219113fc
arshu <arshu@gmail.com>
2023-11-19 21:22:55 +0000

    modified

--- src/main/java/com/iamvickyav/springboot/SpringBootRestWithH2/model/Employee.java
+++ src/main/java/com/iamvickyav/springboot/SpringBootRestWithH2/model/Employee.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "EMPLOYEE")
+@Table(name = "EMpLOYEE")
 public class Employee {
 
     @Column(name = "id")
